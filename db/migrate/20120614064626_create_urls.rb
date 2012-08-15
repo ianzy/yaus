0f734055e66e22261ed9eb75e6b2514530fe4c4d
Yu Zhang <ian7zy@gmail.com>
2012-08-14 23:00:23 -0700

    migration updates

--- db/migrate/20120614064626_create_urls.rb
+++ db/migrate/20120614064626_create_urls.rb
@@ -1,10 +1,11 @@
 class CreateUrls < ActiveRecord::Migration
   def change
-    create_table :urls, :options => "AUTO_INCREMENT = 1000" do |t|
+    create_table :urls do |t|
       t.string :url
       t.string :hash
 
       t.timestamps
     end
+    execute "SELECT setval('customers_id_seq', 1000);"
   end
 end
